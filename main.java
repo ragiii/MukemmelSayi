5b2b1ca96864c60b66a7401b62af2bba52638da3
Sercan S <83725191+ragiii@users.noreply.github.com>
2022-06-07 20:11:20 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
-public class main {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.print("Bir sayı giriniz :");
-        int n = input.nextInt();
+        public class Main {
+            public static void main(String[] args) {
+                Scanner input = new Scanner(System.in);
 
-        int total=0;
+                System.out.print("Bir sayı giriniz :");
+                int n = input.nextInt();
 
-        for(int i=1; i<n; i++){
-            if(n%i==0){
-                total +=i;
+                int total=0;
+
+                for(int i=1; i<n; i++){
+                    if(n%i==0){
+                        total +=i;
+                    }
+                }
+                if(n==total){
+                    System.out.println(n + " Mükemmel Bir Sayı!");
+
+                }
+                else{
+                    System.out.println(n + "Mükemmel Bir Sayı Değil!");
+                }
+
+
+
             }
-        }
-        if(n==total){
-            System.out.println(n + " Mükemmel Bir Sayı!");
 
         }
-        else{
-            System.out.println(n + "Mükemmel Bir Sayı Değil!");
-        }
-
-
-
-    }
-    
-}
